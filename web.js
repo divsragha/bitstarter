3ca9e060ab2b32f21017a8d2a3827e0d1d5c9643
divsragha <divs.raghavan@gmail.com>
2013-07-14 18:59:30 +0000

    Updated web.js to read from index.html

--- web.js
+++ web.js
@@ -1,9 +1,26 @@
 var express = require('express');
+var fs = require("fs");
+var fileName = "index.html";
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+	fs.exists(fileName,function(exists){
+	if(exists){
+		fs.stat(fileName,function(error,stats){
+			fs.open(fileName,"r",function(error,fd){
+			 var buffer = new Buffer(stats.size);
+			 fs.read(fd,buffer,0,buffer.length,null,function(error,bytesRead,buffer){
+				var data = buffer.toString("utf8",0,buffer.length);
+				console.log(data);
+				response.send(data);
+				fs.close(fd);  	
+				});
+			});
+		 });
+	}
+
+  });
 });
 
 var port = process.env.PORT || 5000;
